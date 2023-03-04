baa3e639d6dfeb1c89ae725a6724ea3d24f09dc3
Wilmer Rodriguez <wilmerr09@gmail.com>
2023-03-03 22:02:23 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <div className="text-white absolute inset-0 flex flex-col justify-center items-center">
+      <h1>Vite + React</h1>
+    </div>
+  )
+}
+
+export default App
